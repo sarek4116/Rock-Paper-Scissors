5df4e5c47e648a9f8dcdfe3d57fc40e7812786a2
Aleksander Sar <sarek4116@gmail.com>
2019-05-07 19:05:10 +0200

    add more functions

--- js/script.js
+++ js/script.js
@@ -12,16 +12,42 @@ var playerScore = document.getElementById('player-output')
 var computerScore = document.getElementById('computer-output');
 var rounds = document.getElementById('rounds-output');
 
+// RESULT 
+
+var results = {
+    x: 0,
+    y: 0,
+}
+
+playerScore.innerHTML = results.x;
+computerScore.innerHTML = results.y;
+
+
+// GAME RESET FUNCTION
+
+function reset() {
+    results = {
+        x: 0,
+        y: 0,
+    }
+
+    playerScore.innerHTML = results.x;
+    computerScore.innerHTML = results.y;
+    alerts.innerHTML = ('');
+}
 
 // NEW GAME BUTTON
 
 buttonStart.addEventListener('click', function () {
     var roundAsk = prompt('How many rounds do you want to play ?');
-    if (roundAsk > 0)
+    if (roundAsk > 0) {
         rounds.innerHTML = ('We are playing to ' + roundAsk + ' wins');
-    else
+    } else {
         alert('Zła wartość !')
+    }
+    reset();
 });
+
 
 // FUNCTION WHICH RETURN RANDOM VALUE
 
@@ -29,36 +55,49 @@ function computer() {
     return Math.floor(Math.random() * 3) + 1;
 }
 
-// FUNCTION WHICH SHOW RESULT
-
 // Rock
 function msgRock(random) {
-    if (random === 1)
-        alerts.innerHTML = ('YOU LOSE: you played ROCK, computer played PAPER');
-    else if (random === 2)
-        alerts.innerHTML = ('DRAW !: you played ROCK, computer played ROCK');
-    else
-        alerts.innerHTML = ('YOU WON: you played ROCK, computer played SCISSORS');
+    if (random === 1) {
+        alerts.innerHTML = ('YOU LOSE ! : you played ROCK, computer played PAPER');
+        results.y++
+        computerScore.innerHTML = results.y;
+    } else if (random === 2) {
+        alerts.innerHTML = ('DRAW ! : you played ROCK, computer played ROCK');
+    } else {
+        alerts.innerHTML = ('YOU WON ! : you played ROCK, computer played SCISSORS');
+        results.x++
+        playerScore.innerHTML = results.x;
+    }
 }
 
 // Paper
 function msgPaper(random) {
     if (random === 1)
-        alerts.innerHTML = ('DRAW !: you played PAPER, computer played PAPER');
-    else if (random === 2)
-        alerts.innerHTML = ('YOU WON !: you played PAPER, computer played ROCK');
-    else
-        alerts.innerHTML = ('YOU LOSE !: you played PAPER, computer played SCISSORS');
+        alerts.innerHTML = ('DRAW ! : you played PAPER, computer played PAPER' + '<br>');
+    else if (random === 2) {
+        alerts.innerHTML = ('YOU WON ! : you played PAPER, computer played ROCK' + '<br>');
+        results.x++
+        playerScore.innerHTML = results.x;
+    } else {
+        alerts.innerHTML = ('YOU LOSE ! : you played PAPER, computer played SCISSORS' + '<br>');
+        results.y++
+        computerScore.innerHTML = results.y;
+    }
 }
 
 // Scissors
 function msgScissors(random) {
-    if (random === 1)
-        alerts.innerHTML = ('YOU WON !: you played SCISSORS, computer played PAPER');
-    else if (random === 2)
-        alerts.innerHTML = ('YOU LOSE !: you played SCISSORS, computer played ROCK');
-    else
-        alerts.innerHTML = ('DRAW !: you played SCISSORS, computer played SCISSORS');
+    if (random === 1) {
+        alerts.innerHTML = ('YOU WON ! : you played SCISSORS, computer played PAPER');
+        results.x++
+        playerScore.innerHTML = results.x;
+    } else if (random === 2) {
+        alerts.innerHTML = ('YOU LOSE ! : you played SCISSORS, computer played ROCK');
+        results.y++
+        computerScore.innerHTML = results.y;
+    } else {
+        alerts.innerHTML = ('DRAW ! : you played SCISSORS, computer played SCISSORS');
+    }
 }
 
 // PLAYER MOVES
@@ -98,3 +137,30 @@ paper.addEventListener('click', function () {
 scissors.addEventListener('click', function () {
     playerMoveScissors();
 })
+
+
+// FUNCTION WHICH DISABLED AND ENABLED BUTTONS 
+
+function buttonDisable() {
+    rock.disabled = true;
+    paper.disabled = true;
+    scissors.disabled = true;
+}
+
+function buttonEnabled() {
+    rock.disabled = false;
+    paper.disabled = false;
+    scissors.disabled = false;
+}
+
+// WINNER FUNCTION 
+
+function winner(roundAsk) {
+    if (results.x === roundAsk) {
+        alerts.innerHTML = ("YOU WON THE ENTIRE GAME!!!")
+        buttonDisable();
+    } else if (results.y === roundAsk) {
+        alerts.innerHTML = ("YOU LOSE THE ENTIRE GAME!!!")
+        buttonDisable();
+    }
+}
